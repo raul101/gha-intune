1063bd8cb792277f1cd9779dec8e6ed48cd356ab
murphycraig250 <146171168+murphycraig250@users.noreply.github.com>
2025-07-11 15:03:18 +0100

    Update delete-intune-autopilot.ps1

--- delete-intune-autopilot.ps1
+++ delete-intune-autopilot.ps1
@@ -1,3 +1,4 @@
+#mandatory graph token param
 [CmdletBinding()]
     param
     (
@@ -10,18 +11,26 @@
 #Variables
 $SerialNumber = "CF FE 4E 12"
 $Manufacturer = "Parallels"
-        
+
+#request headers
 $headers = @{
   "Content-Type" = "application/json"
    Authorization = "Bearer {0}" -f $GraphToken}
+#url
 $apiUrl = "https://graph.microsoft.com/v1.0/deviceManagement/windowsAutopilotDeviceIdentities?$filter=contains(serialNumber, $SerialNumber)and contains(manufacturer, $Manufacturer)"
-$method = "GET"
-$AutopilotDevices = Invoke-RestMethod -Uri $apiUrl -Method $method -Headers $headers
+
+#get autopilot devices
+$AutopilotDevices = Invoke-RestMethod -Uri $apiUrl -Method GET -Headers $headers
+
+#for each look to ensure device is correct, unable to use equals in device identities filter
 ForEach ($AutopilotDevice in $AutopilotDevices)
-  {$Serial = $AutopilotDevice.value.serialNumber
-    If ($Serial.StartsWith("Parallels"))# -and $Serial.Endswith("CF FE 4E 12"))
-         {$ID = $AutopilotDevice.value.Id
-         Write-Host $ID`n$Serial
-         #Run delete
+  {$Serial = $AutopilotDevice.value.serialNumber #get serial
+    If ($Serial.StartsWith("Parallels"))# -and $Serial.Endswith("CF FE 4E 12")) #if matches
+         {$ID = $AutopilotDevice.value.Id #set Id
+         #delete device
+         $apiUrl = "https://graph.microsoft.com/v1.0/deviceManagement/windowsAutopilotDeviceIdentities/$ID"
+         Invoke-RestMethod -Uri $apiUrl -Method DELETE -Headers $headers
          }
+    Else
+      {}
   }
