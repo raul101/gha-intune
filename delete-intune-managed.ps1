24cca8b01effb8eec6848fba81ac8c1448b647c2
murphycraig250 <146171168+murphycraig250@users.noreply.github.com>
2025-07-11 14:42:43 +0100

    Update delete-intune-managed.ps1

--- delete-intune-managed.ps1
+++ delete-intune-managed.ps1
@@ -1,1 +1,22 @@
+[CmdletBinding()]
+    param
+    (
+        [parameter(Mandatory)]
+        [ValidateNotNullOrEmpty()]
+        [string]$GraphToken
+    )
+    
+#Variables
+$DeviceName = "UK-AWS-CFFE4E12"
+        
+$headers = @{
+  "Content-Type" = "application/json"
+   Authorization = "Bearer {0}" -f $GraphToken}
+$apiUrl = "https://graph.microsoft.com/v1.0/deviceManagement/managedDevices?$filter=eq(deviceName, $DeviceName)&Select=Id"
+$method = "GET"
+$ManagedDevice = Invoke-RestMethod -Uri $apiUrl -Method $method -Headers $headers
+$ManagedId = $ManagedDevice.value.Id
+Write-Host $ManagedId
 
+#Delete
+#
