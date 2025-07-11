b14278350ea61f590ea97354c8db64eec29e6742
murphycraig250 <146171168+murphycraig250@users.noreply.github.com>
2025-07-11 12:59:33 +0100

    Update delete-intune-autopilot.ps1

--- delete-intune-autopilot.ps1
+++ delete-intune-autopilot.ps1
@@ -1,1 +1,17 @@
-https://graph.microsoft.com/v1.0/deviceManagement/windowsAutopilotDeviceIdentities?$filter=contains(serialNumber,'CF FE 4E 12')and contains(manufacturer,'Parallels')
+#Variables
+$SerialNumber = "CF FE 4E 12"
+$Manufacturer = "Parallels"
+        
+$headers = @{
+  "Content-Type" = "application/json"
+   Authorization = "Bearer {0}" -f $GraphToken}
+$apiUrl = "https://graph.microsoft.com/v1.0/deviceManagement/windowsAutopilotDeviceIdentities?$filter=contains(serialNumber, $SerialNumber)and contains(manufacturer, $Manufacturer)"
+$method = "GET"
+$AutopilotDevices = Invoke-RestMethod -Uri $apiUrl -Method $method -Headers $headers
+ForEach ($AutopilotDevice in $AutopilotDevices)
+  {$Serial = $AutopilotDevice.value.serialNumber
+    If ($Serial.StartsWith("Parallels"))# -and $Serial.Endswith("CF FE 4E 12"))
+         {$ID = $AutopilotDevice.value.Id
+         Write-Host $ID`n$Serial
+         #Run delete}
+  }
